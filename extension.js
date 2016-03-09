eae43d6719fac6edaef7e57d4f0ba990952e8cb3
CoolAcid <jakendall@gmail.com>
2016-03-08 23:54:58 -0500

    Add temp sublistener and streamtip, fix hash

--- extension.js
+++ extension.js
@@ -4,12 +4,13 @@
 module.exports = function(nodecg) {
     // Check that config is present and valid
 
-    var sublistener = nodecg.extensions['lfg-sublistener'];
     var nucleus = nodecg.extensions['lfg-nucleus'];
+//    var streamTip = nodecg.extensions['lfg-streamtip'];
+//    var sublistener = nodecg.extensions['lfg-sublistener'];
 
-    var subcount = nodecg.Replicant('subs', {defaultValue: {current: 0, goal: 100}});
+    var subsRep = nodecg.Replicant('subs', {defaultValue: {current: 0, goal: 100}});
 
     nucleus.on('subscription', function subscription(data) {
-        subcount.value = subcount.value + 1;
+        subsRep.value.current = subcount.value.current + 1;
     });
 };
