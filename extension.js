47e9aeb990c262d860874376598e9d58674548f8
CoolAcid <jakendall@gmail.com>
2016-03-08 23:56:05 -0500

    Add and move replicats

--- extension.js
+++ extension.js
@@ -2,13 +2,14 @@
 'use strict';
 
 module.exports = function(nodecg) {
-    // Check that config is present and valid
+    var subsRep = nodecg.Replicant('subs', {defaultValue: {current: 0, goal: 100}});
+    var TipsRep = nodecg.Replicant('Tips', {defaultValue: {current: 0, goal: 100}});
+    var FollowersRep = nodecg.Replicant('Followers', {defaultValue: {current: 0, goal: 100}});
 
     var nucleus = nodecg.extensions['lfg-nucleus'];
 //    var streamTip = nodecg.extensions['lfg-streamtip'];
 //    var sublistener = nodecg.extensions['lfg-sublistener'];
 
-    var subsRep = nodecg.Replicant('subs', {defaultValue: {current: 0, goal: 100}});
 
     nucleus.on('subscription', function subscription(data) {
         subsRep.value.current = subcount.value.current + 1;
