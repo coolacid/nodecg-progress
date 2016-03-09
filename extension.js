0377f5a82c500b2b42db394db5f1101d547006f1
CoolAcid <jakendall@gmail.com>
2016-03-09 13:00:18 -0500

    revert to streamtip/sublistener if nucleus doesn't exist

--- extension.js
+++ extension.js
@@ -6,15 +6,27 @@ module.exports = function(nodecg) {
     var TipsRep = nodecg.Replicant('tips', {defaultValue: {current: 0, goal: 100, text: ""}});
     var FollowersRep = nodecg.Replicant('followers', {defaultValue: {current: 0, goal: 100}});
 
-    var nucleus = nodecg.extensions['lfg-nucleus'];
-//    var streamTip = nodecg.extensions['lfg-streamtip'];
-//    var sublistener = nodecg.extensions['lfg-sublistener'];
+    if (Object(nodecg.extensions).hasOwnProperty('lfg-nucleus')) {
+        var nucleus = nodecg.extensions['lfg-nucleus'];
 
-    nucleus.on('subscription', function subscription(data) {
-        subsRep.value.current = subsRep.value.current + 1;
-    });
+        nucleus.on('subscription', function subscription(data) {
+            subsRep.value.current = subsRep.value.current + 1;
+        });
 
-    nucleus.on('tip', function subscription(data) {
-        TipsRep.value.current = TipsRep.value.current + data.amount;
-    });
+        nucleus.on('tip', function subscription(data) {
+            TipsRep.value.current = TipsRep.value.current + data.amount;
+        });
+    } else {
+        var streamTip = nodecg.extensions['lfg-streamtip'];
+        var sublistener = nodecg.extensions['lfg-sublistener'];
+
+        streamTip.on('tip',function tip(data) {
+            TipsRep.value.current = TipsRep.value.current + data.amount;
+        });
+
+        sublistener.on('subscription',function subscription(data) {
+            subsRep.value.current = subsRep.value.current + 1;
+        });
+    }
+
 };
