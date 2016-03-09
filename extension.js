dc041d334ccfebb4b74c66f5cd36148865723f0e
CoolAcid <jakendall@gmail.com>
2016-03-08 23:58:26 -0500

    Add extra graphics, change case

--- extension.js
+++ extension.js
@@ -3,13 +3,12 @@
 
 module.exports = function(nodecg) {
     var subsRep = nodecg.Replicant('subs', {defaultValue: {current: 0, goal: 100}});
-    var TipsRep = nodecg.Replicant('Tips', {defaultValue: {current: 0, goal: 100}});
-    var FollowersRep = nodecg.Replicant('Followers', {defaultValue: {current: 0, goal: 100}});
+    var TipsRep = nodecg.Replicant('tips', {defaultValue: {current: 0, goal: 100}});
+    var FollowersRep = nodecg.Replicant('followers', {defaultValue: {current: 0, goal: 100}});
 
     var nucleus = nodecg.extensions['lfg-nucleus'];
 //    var streamTip = nodecg.extensions['lfg-streamtip'];
 //    var sublistener = nodecg.extensions['lfg-sublistener'];
-
 
     nucleus.on('subscription', function subscription(data) {
         subsRep.value.current = subcount.value.current + 1;
