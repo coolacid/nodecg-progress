47e39cd474aae401cb1bf10c775cb568ad9ae7e0
CoolAcid <jakendall@gmail.com>
2016-03-09 11:51:02 -0500

    Fully working version

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -1,25 +1,32 @@
 (function() {
-    var tabs = document.querySelector('#tabs');
-    var subcount = document.querySelector('#subcount');
-    var subgoal = document.querySelector('#subgoal');
-    var setcount = document.querySelector('#setCount');
+    var subcount = document.querySelector('div[data-field="subcount"]');
+    var subgoal = document.querySelector('div[data-field="subgoal"]');
 
-    var countreplicant = nodecg.Replicant('subs');
+    var followercount = document.querySelector('div[data-field="followercount"]');
+    var followergoal = document.querySelector('div[data-field="followergoal"]');
 
-    tabs.selected=0;
+    var tipcount = document.querySelector('div[data-field="tipcount"]');
+    var tipgoal = document.querySelector('div[data-field="tipgoal"]');
+    var tiptext = document.querySelector('span[data-field="tiptext"]');
 
-    tabs.addEventListener('iron-select', function() {
-        console.log("Selected: " + tabs.selected);
+    var subReplicant = nodecg.Replicant('subs');
+    var tipReplicant = nodecg.Replicant('tips');
+    var followerReplicant = nodecg.Replicant('followers');
+
+    subReplicant.on('change', function(oldVal, newVal) {
+        subcount.innerText = newVal.current;
+        subgoal.innerText = newVal.goal;
     });
 
-    setcount.addEventListener('click', function(e) {
-        console.log("Setting to: " + subcount.value)
-        countreplicant.value.current = parseFloat(subcount.value)
-        countreplicant.value.goal = parseFloat(subgoal.value)
+    tipReplicant.on('change', function(oldVal, newVal) {
+        tipcount.innerText = newVal.current;
+        tipgoal.innerText = newVal.goal;
+        tiptext.innerText = newVal.text;
     });
 
-    countreplicant.on('change', function(oldVal, newVal) {
-        subcount.value = newVal.current;
-        subgoal.value = newVal.goal;
+    followerReplicant.on('change', function(oldVal, newVal) {
+        followercount.innerText = newVal.current;
+        followergoal.innerText = newVal.goal;
     });
+
 })();
